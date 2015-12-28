152ebd2a984e47646baccc28dc97aeb833e29941
Vincent Tatan <vincentt.2013@sis.smu.edu.sg>
2015-12-28 15:21:14 +0800

    develop Node js Treehouse Profile Search Project

---
+++ app.js
@@ -0,0 +1,13 @@
+var router = require("./router.js");
+//Problem: we need a simple way to look at user's badge count and Javascript point from a web browser
+//Solution: Use node.js to perform the profile look ups and server our template via HTTP
+
+//1 Create a web server
+var http=require('http');
+http.createServer(function(request,response){
+  router.home(request,response);
+  router.user(request,response);
+}).listen(3000);
+console.log('Server running at http://<workspace-url>');
+
+
